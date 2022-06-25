59e257d8722ef538d5d9a86a5f76f9d002807678
AlfredoRiveros <silvio.riveros.94@gmail.com>
2022-06-24 20:41:46 -0400

    primer commit del portafolio.

---
+++ js/botones.js
@@ -0,0 +1,34 @@
+var botonRepoEncriptador = document.querySelector(".repositorioEncriptador");
+
+botonRepoEncriptador.addEventListener("click", function(event){
+    event.preventDefault();
+    window.open('https://github.com/SilvioAlfredo/-challengeonecodificador2', '_blank');
+});
+
+var botonDemoEncriptador = document.querySelector(".demoEncriptador");
+
+botonDemoEncriptador.addEventListener("click", function(event){
+    event.preventDefault();
+    window.open('https://silvioalfredo.github.io/-challengeonecodificador2/', '_blank');
+});
+
+var botonRepoAhorcado = document.querySelector(".repositorioAhorcado");
+
+botonRepoAhorcado.addEventListener("click", function(event){
+    event.preventDefault();
+    window.open('https://github.com/SilvioAlfredo/AhorcadoONERiverosAlfredo', '_blank');
+});
+
+var botonDemoAhorcado = document.querySelector(".demoAhorcado");
+
+botonDemoAhorcado.addEventListener("click", function(event){
+    event.preventDefault();
+    window.open('https://silvioalfredo.github.io/AhorcadoONERiverosAlfredo/', '_blank');
+});
+
+var botonRepoXilofon = document.querySelector(".repositorioXilofon");
+
+botonRepoXilofon.addEventListener("click", function(event){
+    event.preventDefault();
+    window.open('https://github.com/SilvioAlfredo/Xilofon', '_blank');
+});
