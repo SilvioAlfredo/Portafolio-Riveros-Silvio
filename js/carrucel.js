51e60633188df820a57b7dfacbeae21f06d31876
Alfredo Riveros <silvio.riveros.94@gmail.com>
2024-06-08 16:08:49 -0300

    completado parte del diseño y el js

--- js/carrucel.js
+++ js/carrucel.js
@@ -1,15 +1,8 @@
-// Selecciono el elemento en html al que añadiré la estructura generada con js
-const contenedor = document.querySelector('.skills');
-const contenedorHobbies = document.querySelector('#contenedorHobbies');
+const contenedorFormacion = document.querySelector('.formaciones');
+const rutaArchivoJSONFormacion = "imgFormaciones.json";
 
-// Alojo la ruta del archivo json que contiene los recursos en una constante
-const rutaArchivoJSON = 'iconoSkills.json';
-const rutaArchivoJSONHobbies = 'iconoHobbies.json';
-
-// Asigno a constante el nombre de una clase css
-const classSkills = ".skillsItem";
-const classImgHabilidades = "imagenhabilidades";
-
+const classFormacion = ".formaciónsItem"
+const classImgFormacion = "imagenFormacion";
 
 // función con 4 parámetros que genera parte del html
 function generarEstructuras(ruta,contenedor,claseli,claseImg){
@@ -21,10 +14,12 @@ function generarEstructuras(ruta,contenedor,claseli,claseImg){
     for (const dato of data) { 
       const titulo = dato.title; 
       const src = dato.src;
+      const subtitle = dato.subtitle;
+      const location = dato.location;
       // En esta línea se crea una constante a la cual se le asigna la creación de un elemento <li>
       const nuevoLi = document.createElement('li');
       // nuevoLi accede a la función inner... y se le asigna una estructura de html
-      nuevoLi.innerHTML = `<img class="${claseImg}" src="${src}" alt="${titulo}"><h4 class="stringHabilidades">${titulo}</h4>`;
+      nuevoLi.innerHTML = `<img class="${claseImg}" src="${src}" alt="${titulo}"><h4 class="stringHabilidades"><strong>${titulo}</strong><br>${subtitle}<br>${location}</h4>`;
       nuevoLi.classList.add(claseli);
       contenedor.appendChild(nuevoLi);
     }
@@ -33,6 +28,4 @@ function generarEstructuras(ruta,contenedor,claseli,claseImg){
   Usa una función flecha que imprime un error por consola y cita err*/
   .catch(err => console.error('Error al leer el archivo JSON:', err));
 }
-// Estas son llamadas a la función para pasarle los correspondientes parámetros
-generarEstructuras(rutaArchivoJSON, contenedor,classSkills,classImgHabilidades)
-generarEstructuras(rutaArchivoJSONHobbies, contenedorHobbies,classSkills, classImgHabilidades)
+generarEstructuras(rutaArchivoJSONFormacion, contenedorFormacion, classFormacion, classImgFormacion)
